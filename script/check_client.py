0d4b3e890f059fd34708f085435d71b8e3304ede
Doug Wiegley <doug@parksidesoftware.com>
2014-05-07 10:44:02 -0600

    script to check ax client conn

---
+++ script/check_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import logging
+import sys
+
+sys.path.insert(0, "./test")
+sys.path.insert(0, "./neutron/neutron/services/loadbalancer/drivers")
+
+from a10networks.acos_client import A10Client
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+c = A10Client()
