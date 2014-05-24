b9a3607f96c1108781e36d28c6a936d1c32b1d95
Doug Wiegley <doug@parksidesoftware.com>
2014-05-23 23:08:14 -0600

    update client check for new config

--- script/check_client.py
+++ script/check_client.py
@@ -6,7 +6,9 @@ import sys
 sys.path.insert(0, "./neutron/neutron/services/loadbalancer/drivers")
 
 from a10networks.acos_client import A10Client
+import a10networks.a10_config as a10_config
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-c = A10Client()
+config = a10_config.A10Config()
+c = A10Client(config, version_check=True)
